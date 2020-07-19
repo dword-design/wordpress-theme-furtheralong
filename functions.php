824b50b0f0579ddf480c2eb8469cdea8561d2a86
foo <foo@bar.de>
2020-07-19 11:51:14 +0200

    fix: fix styles

--- functions.php
+++ functions.php
@@ -1,18 +1,23 @@
 <?php
 
-	function customizer($wp_customize) {
+function enqueue_scripts() {
+	wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+	wp_enqueue_style('style', get_stylesheet_directory_uri() . '/css/style.css');
+}
+add_action('wp_enqueue_scripts', 'enqueue_scripts');
 
-		$wp_customize->add_section('logo', array(
-			'title' => __('Logo', 'dusktodawn')
-		));
-		
-		$wp_customize->add_setting('logo');
-		$wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'logo', array(
-			'label' => 'Logo',
-			'section' => 'logo',
-			'settings' => 'logo',
-		)));
-	}
+function customizer($wp_customize) {
+
+	$wp_customize->add_section('logo', array(
+		'title' => __('Logo', 'dusktodawn')
+	));
 	
-	add_action('customize_register', 'customizer');
-?>
+	$wp_customize->add_setting('logo');
+	$wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'logo', array(
+		'label' => 'Logo',
+		'section' => 'logo',
+		'settings' => 'logo',
+	)));
+}
+
+add_action('customize_register', 'customizer');
